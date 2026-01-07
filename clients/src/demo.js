18e8cda54c06933c0f93dfb12cb41b75b31a2d6c
iliass7777 <meeiliass6@gmail.com>
2026-01-07 21:50:00 +0100

    updatre logic

---
+++ clients/src/demo.js
@@ -0,0 +1,9 @@
+export default function demo(){
+    return(
+        <div id="demo">   
+            <h1>Hello World</h1>
+            <p>This is a demo component</p>
+            <button>Click me</button>
+        </div>
+    )
+}
---
+++ server/index.js
@@ -0,0 +1,16 @@
+const fastify=require('fastify')({logger:true});
+require('dotenv').config();
+
+
+fastify.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+fastify.post('/api/test',(req,res)=>{
+    return {message:'Hello World'};
+})
+fastify.listen({port:process.env.PORT || 3000},(err,address)=>{
+    if(err) throw err;
+    console.log(`Server is running on ${address}`);
+});
+
+
